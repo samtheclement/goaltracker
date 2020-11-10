5ba671431c4ade2af1772b5856ecd4ed4e85399b
SamtheClement <sam.clement@umt.edu>
2020-11-09 21:48:37 -0700

    more heroku updates

--- goaltracker_project/settings.py
+++ goaltracker_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f!67+tf0ffuee8t1uom2lseec%+d)$=#tvk^_$kok$$#tja-26'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
